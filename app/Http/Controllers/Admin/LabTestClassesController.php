1ae728a69ebaf4c11b3984276382d794ab63a5a6
davidokumbo <davidokumbo@gmail.com>
2024-12-17 16:46:30 +0300

    changes on six controllers image tests and lab tests by david

--- app/Http/Controllers/Admin/LabTestClassesController.php
+++ app/Http/Controllers/Admin/LabTestClassesController.php
@@ -4,8 +4,174 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Exceptions\AlreadyExistsException;
+use App\Exceptions\NotFoundException;
+use App\Models\Admin\LabTestClass;
+use App\Models\User;
+use App\Models\UserActivityLog;
+use App\Utils\APIConstants;
+use Carbon\Carbon;
+
+
 
 class LabTestClassesController extends Controller
 {
-    //
+
+        //create
+        public function createLabTestClass(Request $request){
+            $request->validate([
+                'name' => 'required|string|min:1|max:255|unique:diagnosis,name',
+                'description'=>'string|min:1|max:255'            
+            ]);
+    
+    
+            LabTestClass::create([
+                'name' => $request->name, 
+                'description' => $request->description,
+                'created_by' => User::getLoggedInUserId()
+            ]);
+    
+            UserActivityLog::createUserActivityLog(APIConstants::NAME_CREATE, "Created a lab test class with name: ". $request->name);
+    
+            return response()->json(
+                LabTestClass::selectLabTestClass(null, $request->name)
+            ,200);
+    
+        }
+    
+        //update
+        public function updateLabTestClass(Request $request){
+            $request->validate([
+                'id' => 'required|integer|exists:diagnosis,id',
+                'name' => 'required|string|min:1|max:255',
+                'description'=>'string|min:1|max:255'
+                
+            ]);
+    
+            $existing = LabTestClass::selectLabTestClass(null, $request->name);
+    
+            count($existing) > 0  && $existing[0]['id'] != $request->id ? throw new AlreadyExistsException(APIConstants::NAME_LAB_TEST_CLASS) : null;
+    
+    
+            LabTestClass::where('id', $request->id)
+                ->update([
+                    'name' => $request->name,
+                    'description' => $request->description,
+                    'updated_at' => Carbon::now(),
+                    'updated_by' => User::getLoggedInUserId(),
+                    'approved_by' => null,
+                    'approved_at' => null
+            ]);
+    
+            UserActivityLog::createUserActivityLog(APIConstants::NAME_UPDATE, "Updated a lab test class with name: ". $request->name);
+    
+            return response()->json(
+                LabTestClass::selectLabTestClass($request->id, null)
+            ,200);
+    
+        }
+    
+        //     //Get one 
+        public function getSingleLabTestClass(Request $request){
+    
+            $labTestClass = LabTestClass::selectLabTestClass($request->id, $request->name);
+    
+            count($labTestClass) < 1 ? throw new NotFoundException(APIConstants::NAME_LAB_TEST_CLASS) : null ;
+    
+            UserActivityLog::createUserActivityLog(APIConstants::NAME_GET, "Fetched a lab test class with name: ". $labTestClass[0]['name']);
+    
+            return response()->json(
+                $labTestClass
+            ,200);
+        }
+    
+    
+        //getting all
+        public function getAllLabTestClass(){
+    
+            $labTestClass = LabTestClass::selectLabTestClass(null, null);
+    
+            UserActivityLog::createUserActivityLog(APIConstants::NAME_GET, "Fetched all lab test classes");
+    
+            return response()->json(
+                $labTestClass
+            ,200);
+        }
+    
+        //approve
+        public function approveLabTestClass($id){
+    
+            count(LabTestClass::selectLabTestClass($id, null)) < 1 ? throw new NotFoundException(APIConstants::NAME_LAB_TEST_CLASS) : null;
+    
+            LabTestClass::where('id', $id)
+                ->update([
+                    'approved_by' => User::getLoggedInUserId(),
+                    'approved_at' => Carbon::now()
+            ]);
+    
+    
+            UserActivityLog::createUserActivityLog(APIConstants::NAME_APPROVE, "Approved a lab test class with id: ".$id);
+    
+            return response()->json(
+                LabTestClass::selectLabTestClass($id, null)
+            ,200);
+    
+        }
+    
+        //soft delete
+        public function softDeleteLabTestClass($id){
+                
+            count(LabTestClass::selectLabTestClass($id, null)) < 1 ? throw new NotFoundException(APIConstants::NAME_LAB_TEST_CLASS) : null;
+            
+            LabTestClass::where('id', $id)
+                    ->update([
+                        'deleted_at' => now(),
+                        'deleted_by' => User::getLoggedInUserId(),
+                    ]);
+    
+    
+            UserActivityLog::createUserActivityLog(APIConstants::NAME_SOFT_DELETE, "Trashed a lab test class with id: ". $id);
+    
+            return response()->json(
+                []
+            ,200);
+        }
+    
+        //restore
+        public function restoreSoftDeleted($id){ 
+            
+            count(LabTestClass::where('id', $id)->whereNotNull('deleted_by')->get()) < 1 ? throw new NotFoundException(APIConstants::NAME_LAB_TEST_CLASS) : null;
+            
+            LabTestClass::where('id', $id)
+                    ->update([
+                        'approved_at' => null,
+                        'approved_by' => null,
+                        'deleted_at' => null,
+                        'deleted_by' => null,
+                    ]);
+    
+    
+            UserActivityLog::createUserActivityLog(APIConstants::NAME_RESTORE, "Restored a lab test class with id: ". $id);
+    
+            return response()->json(
+                LabTestClass::selectLabTestClass($id, null)
+            ,200);
+        }
+    
+        //permanently delete
+        public function permanentlyDelete($id){
+                
+            count(LabTestClass::where('id', $id)->get()) < 1 ? throw new NotFoundException(APIConstants::NAME_LAB_TEST_CLASS) : null;
+            
+            LabTestClass::destroy($id);
+    
+    
+            UserActivityLog::createUserActivityLog(APIConstants::NAME_PERMANENT_DELETE, "Deleted a lab test class with id: ". $id);
+    
+            return response()->json(
+                []
+            ,200);
+        }
+
+
 }
--- app/Http/Controllers/Admin/LabTestTypesController.php
+++ app/Http/Controllers/Admin/LabTestTypesController.php
@@ -1,11 +1,175 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
-
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Exceptions\AlreadyExistsException;
+use App\Exceptions\NotFoundException;
+use App\Models\Admin\LabTestType;
+use App\Models\User;
+use App\Models\UserActivityLog;
+use App\Utils\APIConstants;
+use Carbon\Carbon;
+
+
 
 class LabTestTypesController extends Controller
 {
-    //
+    
+     //create
+        public function createLabTestType(Request $request){
+            $request->validate([
+                'name' => 'required|string|min:1|max:255|unique:diagnosis,name',
+                'description'=>'string|min:1|max:255'            
+            ]);
+    
+    
+            LabTestType::create([
+                'name' => $request->name, 
+                'description' => $request->description,
+                'created_by' => User::getLoggedInUserId()
+            ]);
+    
+            UserActivityLog::createUserActivityLog(APIConstants::NAME_CREATE, "Created a lab test type with name: ". $request->name);
+    
+            return response()->json(
+                LabTestType::selectLabTestTypes(null, $request->name)
+            ,200);
+    
+        }
+    
+        //update
+        public function updateLabTestType(Request $request){
+            $request->validate([
+                'id' => 'required|integer|exists:diagnosis,id',
+                'name' => 'required|string|min:1|max:255',
+                'description'=>'string|min:1|max:255'
+                
+            ]);
+    
+            $existing = LabTestType::selectLabTestTypes(null, $request->name);
+    
+            count($existing) > 0  && $existing[0]['id'] != $request->id ? throw new AlreadyExistsException(APIConstants::NAME_LAB_TEST_TYPE) : null;
+    
+    
+            LabTestType::where('id', $request->id)
+                ->update([
+                    'name' => $request->name,
+                    'description' => $request->description,
+                    'updated_at' => Carbon::now(),
+                    'updated_by' => User::getLoggedInUserId(),
+                    'approved_by' => null,
+                    'approved_at' => null
+            ]);
+    
+            UserActivityLog::createUserActivityLog(APIConstants::NAME_UPDATE, "Updated a lab test type  with name: ". $request->name);
+    
+            return response()->json(
+                LabTestType::selectLabTestTypes($request->id, null)
+            ,200);
+    
+        }
+    
+        //     //Get one 
+        public function getSingleLabTestType(Request $request){
+    
+            $labTestType = LabTestType::selectLabTestTypes($request->id, $request->name);
+    
+            count($labTestType) < 1 ? throw new NotFoundException(APIConstants::NAME_LAB_TEST_TYPE) : null ;
+    
+            UserActivityLog::createUserActivityLog(APIConstants::NAME_GET, "Fetched a lab test type with name: ". $labTestType[0]['name']);
+    
+            return response()->json(
+                $labTestType
+            ,200);
+        }
+    
+    
+        //getting all
+        public function getAllLabTestType(){
+    
+            $labTestType = LabTestType::selectLabTestTypes(null, null);
+    
+            UserActivityLog::createUserActivityLog(APIConstants::NAME_GET, "Fetched all lab test types");
+    
+            return response()->json(
+                $labTestType
+            ,200);
+        }
+    
+        //approve
+        public function approveLabTestType($id){
+    
+            count(LabTestType::selectLabTestTypes($id, null)) < 1 ? throw new NotFoundException(APIConstants::NAME_LAB_TEST_TYPE) : null;
+    
+            LabTestType::where('id', $id)
+                ->update([
+                    'approved_by' => User::getLoggedInUserId(),
+                    'approved_at' => Carbon::now()
+            ]);
+    
+    
+            UserActivityLog::createUserActivityLog(APIConstants::NAME_APPROVE, "Approved a lab test type with id: ".$id);
+    
+            return response()->json(
+                LabTestType::selectLabTestTypes($id, null)
+            ,200);
+    
+        }
+    
+        //soft delete
+        public function softDeleteLabTestType($id){
+                
+            count(LabTestType::selectLabTestTypes($id, null)) < 1 ? throw new NotFoundException(APIConstants::NAME_LAB_TEST_TYPE) : null;
+            
+            LabTestType::where('id', $id)
+                    ->update([
+                        'deleted_at' => now(),
+                        'deleted_by' => User::getLoggedInUserId(),
+                    ]);
+    
+    
+            UserActivityLog::createUserActivityLog(APIConstants::NAME_SOFT_DELETE, "Trashed a lab test type with id: ". $id);
+    
+            return response()->json(
+                []
+            ,200);
+        }
+    
+        //restore
+        public function restoreSoftDeleted($id){ 
+            
+            count(LabTestType::where('id', $id)->whereNotNull('deleted_by')->get()) < 1 ? throw new NotFoundException(APIConstants::NAME_LAB_TEST_TYPE) : null;
+            
+            LabTestType::where('id', $id)
+                    ->update([
+                        'approved_at' => null,
+                        'approved_by' => null,
+                        'deleted_at' => null,
+                        'deleted_by' => null,
+                    ]);
+    
+    
+            UserActivityLog::createUserActivityLog(APIConstants::NAME_RESTORE, "Restored a lab test type with id: ". $id);
+    
+            return response()->json(
+                LabTestType::selectLabTestTypes($id, null)
+            ,200);
+        }
+    
+        //permanently delete
+        public function permanentlyDelete($id){
+                
+            count(LabTestType::where('id', $id)->get()) < 1 ? throw new NotFoundException(APIConstants::NAME_LAB_TEST_TYPE) : null;
+            
+            LabTestType::destroy($id);
+    
+    
+            UserActivityLog::createUserActivityLog(APIConstants::NAME_PERMANENT_DELETE, "Deleted a lab test type with id: ". $id);
+    
+            return response()->json(
+                []
+            ,200);
+        }
+
 }
