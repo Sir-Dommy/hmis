61db23283d3b5c39e9deae1f57b2c5fc48595812
Sir-Dommy <dominickyengo2017@gmail.com>
2024-12-18 13:31:49 +0300

    relationships in patients

--- app/Models/Patient/Patient.php
+++ app/Models/Patient/Patient.php
@@ -68,7 +68,7 @@ class Patient extends Model
             'approvedBy:id,email',
             'insuranceDetails:id,patient_id,member_validity', 
             'visits',
-            'visits.vitals'
+            //'visits.vitals'
         ])->whereNull('patients.deleted_by');
 
         if($id != null){
@@ -126,7 +126,7 @@ class Patient extends Model
             'insurance_details' => $patient->insuranceDetails,   
             'chronic_diseases' => $patient->chronicDiseases,
             'visits' => $patient->visits,
-            'vitals' => $patient->visits->vitals,
+            //'vitals' => $patient->visits->vitals,
             'created_by' => $patient->createdBy ? $patient->createdBy->email : null,
             'created_at' => $patient->created_at,
             'updated_by' => $patient->updatedBy ? $patient->updatedBy->email : null,
