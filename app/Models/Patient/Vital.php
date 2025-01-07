66712ef635df1bbae2ed7b9d5a185dd076b89d51
Sir-Dommy <dominickyengo2017@gmail.com>
2025-01-07 15:16:44 +0300

    adding custom functions to relate to users table

--- app/Models/Patient/Vital.php
+++ app/Models/Patient/Vital.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Patient;
 
+use App\Utils\CustomUserRelations;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,6 +10,8 @@ class Vital extends Model
 {
 
     use HasFactory;
+
+    use CustomUserRelations;
 
     protected $table = "vitals";
 
