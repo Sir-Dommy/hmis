f92dc9f5742f2e803120f33de5451a0204cd3a26
Sir-Dommy <dominickyengo2017@gmail.com>
2025-01-20 12:25:24 +0300

    commented on insurance relations selection

--- app/Models/Patient/Patient.php
+++ app/Models/Patient/Patient.php
@@ -138,10 +138,12 @@ class Patient extends Model
             ->orWhere('patients.firstname', 'LIKE', '%'.$value.'%')
             ->orWhere('patients.lastname', 'LIKE', '%'.$value.'%')
             ->orWhere('patients.id_no', 'LIKE', '%'.$value.'%')
-            ->orWhere('patients.patient_code', 'LIKE', '%'.$value.'%')
-            ->orWhereHas('insuranceDetails', function ($query) use ($value) {
-                $query->where('insurance_details.principal_member_number', 'LIKE', '%'.$value.'%'); 
-            });
+            ->orWhere('patients.patient_code', 'LIKE', '%'.$value.'%');
+
+
+            // ->orWhereHas('insuranceDetails', function ($query) use ($value) {
+            //     $query->where('insurance_details.principal_member_number', 'LIKE', '%'.$value.'%'); 
+            // })
 
         
         $paginated_patients = $patients_query->paginate(10);
