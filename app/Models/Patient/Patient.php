cf92b13b4f97abc9fad4cc254d693ea816f31917
Sir-Dommy <dominickyengo2017@gmail.com>
2025-01-20 12:52:57 +0300

    nesting selection patient search results

--- app/Models/Patient/Patient.php
+++ app/Models/Patient/Patient.php
@@ -107,8 +107,6 @@ class Patient extends Model
         }
 
 
-        echo $patients_query->toSql();
-
         return $patients_query->get()->map(function ($patient) {
             $patient_details = Patient::mapResponse($patient);
 
@@ -135,9 +133,21 @@ class Patient extends Model
             'visits.scheme:id,name',
             'visits.vitals:id,visit_id,weight,blood_pressure,blood_glucose,height,blood_type,disease,allergies,nursing_remarks'
         ])->whereNull('patients.deleted_by')
-            ->orWhere('patients.id', 'LIKE', '%' . $value . '%');
+            ->where(function ($query) use ($value) {
+            $query->whereHas('insuranceDetails', function ($query) use ($value) {
+                $query->where('insurance_details.principal_member_number', 'LIKE', '%' . $value . '%');
+            })
+            ->orWhere('patients.id', 'LIKE', '%' . $value . '%')
+            ->orWhere('patients.email', 'LIKE', '%'.$value.'%')
+            ->orWhere('patients.firstname', 'LIKE', '%'.$value.'%')
+            ->orWhere('patients.lastname', 'LIKE', '%'.$value.'%')
+            ->orWhere('patients.id_no', 'LIKE', '%'.$value.'%')
+            ->orWhere('patients.patient_code', 'LIKE', '%'.$value.'%');
+        });
 
 
+
+            // ->orWhere('patients.id', 'LIKE', '%' . $value . '%')
             // ->orWhere('patients.email', 'LIKE', '%'.$value.'%')
             // ->orWhere('patients.firstname', 'LIKE', '%'.$value.'%')
             // ->orWhere('patients.lastname', 'LIKE', '%'.$value.'%')
