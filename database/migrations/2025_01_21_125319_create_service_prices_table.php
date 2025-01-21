30fbeca16386e355a64d36e6df7ee3bc86d7f589
Sir-Dommy <dominickyengo2017@gmail.com>
2025-01-21 14:49:57 +0300

    creating migrations for services, service_prices and consultation_categories tables

---
+++ database/migrations/2025_01_21_125319_create_service_prices_table.php
@@ -0,0 +1,142 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('service_prices', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('service_id');
+            $table->unsignedBigInteger('department_id')->nullable();
+            $table->unsignedBigInteger('consultation_category_id')->nullable();
+            $table->unsignedBigInteger('clinic_id')->nullable();
+            $table->unsignedBigInteger('payment_type_id');
+            $table->unsignedBigInteger('scheme_id')->nullable();
+            $table->unsignedBigInteger('scheme_type_id')->nullable();
+            $table->unsignedBigInteger('visit_type_id')->nullable();
+            $table->unsignedBigInteger('doctor_id')->nullable();
+            $table->time('price_applies_from')->nullable();
+            $table->time('price_applies_to')->nullable();
+            $table->double('duration', 8, 4)->nullable();
+            $table->unsignedBigInteger('lab_test_type_id')->nullable();
+            $table->unsignedBigInteger('image_test_type_id')->nullable();
+            $table->unsignedBigInteger('drug_id')->nullable();
+            $table->unsignedBigInteger('brand_id')->nullable();
+            $table->unsignedBigInteger('branch_id')->nullable();
+            $table->unsignedBigInteger('building_id')->nullable();
+            $table->unsignedBigInteger('wing_id')->nullable();
+            $table->unsignedBigInteger('ward_id')->nullable();
+            $table->double('price', 8, 4);
+            $table->unsignedBigInteger('created_by');
+            $table->unsignedBigInteger('updated_by')->nullable();
+            $table->unsignedBigInteger('approved_by')->nullable();
+            $table->timestamp('approved_at')->nullable();
+            $table->unsignedBigInteger('disabled_by')->nullable();
+            $table->timestamp('disabled_at')->nullable();
+            $table->unsignedBigInteger('deleted_by')->nullable();
+            $table->timestamp('deleted_at')->nullable();
+            $table->timestamps();
+
+            $table->foreign('service_id') // Column name
+                  ->references('id') // Target column in the parent table
+                  ->on('services') // Parent table
+                  ->onDelete('cascade');
+
+            $table->foreign('department_id') // Column name
+                ->references('id') // Target column in the parent table
+                ->on('departments') // Parent table
+                ->onDelete('cascade');
+
+            $table->foreign('consultation_category_id') // Column name
+                ->references('id') // Target column in the parent table
+                ->on('consultation_categories') // Parent table
+                ->onDelete('cascade');
+
+            $table->foreign('clinic_id') // Column name
+                ->references('id') // Target column in the parent table
+                ->on('clinics') // Parent table
+                ->onDelete('cascade');
+
+            $table->foreign('payment_type_id') // Column name
+                ->references('id') // Target column in the parent table
+                ->on('payment_types') // Parent table
+                ->onDelete('cascade');
+
+            $table->foreign('scheme_id') // Column name
+                ->references('id') // Target column in the parent table
+                ->on('schemes') // Parent table
+                ->onDelete('cascade');
+
+            $table->foreign('scheme_type_id') // Column name
+                ->references('id') // Target column in the parent table
+                ->on('scheme_types') // Parent table
+                ->onDelete('cascade');
+
+            $table->foreign('visit_type_id') // Column name
+                ->references('id') // Target column in the parent table
+                ->on('visit_types') // Parent table
+                ->onDelete('cascade');
+
+            $table->foreign('doctor_id') // Column name
+                ->references('id') // Target column in the parent table
+                ->on('doctors') // Parent table
+                ->onDelete('cascade');
+
+            $table->foreign('lab_test_type_id') // Column name
+                ->references('id') // Target column in the parent table
+                ->on('lab_test_types') // Parent table
+                ->onDelete('cascade');
+
+            $table->foreign('image_test_type_id') // Column name
+                ->references('id') // Target column in the parent table
+                ->on('image_test_types') // Parent table
+                ->onDelete('cascade');
+
+            $table->foreign('drug_id') // Column name
+                ->references('id') // Target column in the parent table
+                ->on('drugs') // Parent table
+                ->onDelete('cascade');
+
+            $table->foreign('brand_id') // Column name
+                ->references('id') // Target column in the parent table
+                ->on('brands') // Parent table
+                ->onDelete('cascade');
+
+            $table->foreign('created_by') // Column name
+                ->references('id') // Target column in the parent table
+                ->is_null(false)
+                ->on('users') // Parent table
+                ->onDelete('cascade');
+
+            $table->foreign('updated_by') // Column name
+                ->references('id') // Target column in the parent table
+                ->on('users') // Parent table
+                ->onDelete('cascade');
+
+            $table->foreign('approved_by') // Column name
+                ->references('id') // Target column in the parent table
+                ->on('users') // Parent table
+                ->onDelete('cascade');
+
+            $table->foreign('deleted_by') // Column name
+                ->references('id') // Target column in the parent table
+                ->on('users') // Parent table
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('service_prices');
+    }
+};
