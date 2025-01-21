76b6157b9f0e17844a190fe4047e8432fa507731
Sir-Dommy <dominickyengo2017@gmail.com>
2025-01-21 15:26:12 +0300

    creating service relateed models

---
+++ app/Models/Admin/ServiceRelated/Building.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Admin\ServiceRelated;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Building extends Model
+{
+    use HasFactory;
+}
