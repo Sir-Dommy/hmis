7a77d3733e66ad1d1f1bed164722e4fcbedc7fb7
Sir-Dommy <dominickyengo2017@gmail.com>
2025-01-21 10:51:31 +0300

    partly updating a visit

--- app/Http/Controllers/Patient/VisitController.php
+++ app/Http/Controllers/Patient/VisitController.php
@@ -112,52 +112,38 @@ class VisitController extends Controller
         $request->validate([
             'id' => 'required|exists:visits,id',
             'patient_id' => 'required|exists:patients,id',
-            'claim_number' => 'required|string|min:1|max:255',
-            'amount'=>'required|numeric|min:0|',
             'department' => 'required|string|exists:departments,name',
             'clinic'=>'required|string|exists:clinics,name',
-            'visit_type'=>'required|string|min:1|max:255',
-            'scheme' => 'required|string|exists:schemes,name',
-            'fee_type'=>'required|string|exists:payment_types,name',
-            'image' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048', // Allowed formats and max size 2MB
-        
+            'visit_type'=>'required|string|exists:visit_types,name',
+            'schemes' => 'nullable',
+            'payment_types'=>'required',
         ]);
-
-        //store image first 
-        // Get the uploaded file
-        $path = null;
-        if($request->file('image')){
-            $image = $request->file('image');
-
-            // Generate a new unique name for the image
-            $newName = uniqid() . '.' . $image->getClientOriginalExtension();
-    
-            // Store the image in the public folder
-            $path = $image->move(public_path('images/claims'), $newName);
-        }
         
 
         $department = Department::where('name', $request->department)->get("id");
         $clinic = Clinic::where('name', $request->clinic)->get("id");
-        $scheme = Scheme::where('name', $request->scheme)->get("id");
-        $fee_type = PaymentType::where('name', $request->fee_type)->get("id");
+        $visit_type = VisitType::selectVisitTypes(null, $request->visit_type);
 
-        // return response()->file($path);
-        // echo $department[0]['id'];
-        Visit::where('id', $request->id)
+        DB::beginTransaction();
+
+        try{
+            Visit::where('id', $request->id)
             ->update([
-                'patient_id' => $request->patient_id,
-                'claim_number' => $request->claim_number, 
-                'amount' => $request->amount,
-                'department_id'=>$department[0]['id'],
-                'clinic_id'=>$clinic[0]['id'], 
-                'visit_type' => $request->visit_type,
-                'scheme_id' => $scheme[0]['id'],
-                'fee_type'=>$fee_type[0]['id'],
-                'stage'=>0,
-                'document_path'=>$path,
-                'created_by' => User::getLoggedInUserId()
+                'patient_id' => $request->patient_id, 
+                'visit_type_id' => $visit_type[0]['id'],
+                'stage'=> 0,
+                'updated_by' => User::getLoggedInUserId()
             ]);
+
+        
+            DB::commit();
+        }
+
+        catch(Exception $e){
+            DB::rollBack();
+
+            throw new Exception($e);
+        }
 
         UserActivityLog::createUserActivityLog(APIConstants::NAME_UPDATE, "Updated a visit with id: ". $request->id);
 
