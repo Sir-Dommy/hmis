5782e0e8ba124ae6ff7ce5366647172f24c7c9d2
Sir-Dommy <dominickyengo2017@gmail.com>
2025-01-21 16:19:12 +0300

    adding logic for service related models

--- app/Models/Admin/ServiceRelated/Building.php
+++ app/Models/Admin/ServiceRelated/Building.php
@@ -2,10 +2,35 @@
 
 namespace App\Models\Admin\ServiceRelated;
 
+use App\Models\Branch;
+use App\Utils\CustomUserRelations;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Building extends Model
 {
     use HasFactory;
+
+    use CustomUserRelations;
+
+    protected $table = 'buildings';
+
+    protected $fillable = [
+        'name',
+        'description',
+        'branch_id',
+        'created_by',
+        'updated_by',
+        'approved_by',
+        'approved_at',
+        'disabled_by',
+        'disabled_at',
+        'deleted_by',
+        'deleted_at'
+    ];
+
+    public function branch()
+    {
+        return $this->belongsTo(Branch::class, 'branch_id');
+    }
 }
--- app/Models/Admin/ServiceRelated/Service.php
+++ app/Models/Admin/ServiceRelated/Service.php
@@ -2,10 +2,30 @@
 
 namespace App\Models\Admin\ServiceRelated;
 
+use App\Utils\CustomUserRelations;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Service extends Model
 {
     use HasFactory;
+
+    use CustomUserRelations;
+
+    protected $table = 'services';
+
+    protected $fillable = [
+        'name',
+        'description',
+        'created_by',
+        'updated_by',
+        'approved_by',
+        'approved_at',
+        'disabled_by',
+        'disabled_at',
+        'deleted_by',
+        'deleted_at'
+    ];
+
+
 }
