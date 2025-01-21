a2e1776d010d559ca7b07b25b26154be3282c954
Sir-Dommy <dominickyengo2017@gmail.com>
2025-01-21 22:11:08 +0300

    part debug of service price model and controller

--- app/Http/Controllers/Admin/ServiceRelated/ServicePriceController.php
+++ app/Http/Controllers/Admin/ServiceRelated/ServicePriceController.php
@@ -3,9 +3,352 @@
 namespace App\Http\Controllers\Admin\ServiceRelated;
 
 use App\Http\Controllers\Controller;
+use App\Models\Admin\Brand;
+use App\Models\Admin\Clinic;
+use App\Models\Admin\ConsultationType;
+use App\Models\Admin\Department;
+use App\Models\Admin\Drug;
+use App\Models\Admin\Employee;
+use App\Models\Admin\ImageTestType;
+use App\Models\Admin\LabTestType;
+use App\Models\Admin\PaymentType;
+use App\Models\Admin\Scheme;
+use App\Models\Admin\SchemeTypes;
+use App\Models\Admin\ServiceRelated\Building;
+use App\Models\Admin\ServiceRelated\ConsultationCategory;
+use App\Models\Admin\ServiceRelated\Office;
+use App\Models\Admin\ServiceRelated\Service;
+use App\Models\Admin\ServiceRelated\ServicePrice;
+use App\Models\Admin\ServiceRelated\Ward;
+use App\Models\Admin\ServiceRelated\Wing;
+use App\Models\Admin\VisitType;
+use App\Models\Branch;
+use App\Models\UserActivityLog;
+use App\Utils\APIConstants;
 use Illuminate\Http\Request;
 
 class ServicePriceController extends Controller
 {
-    //
+    //get all services
+    public function getAllServices(){        
+
+        $all = ServicePrice::selectServicePrice(null, null, null, null, null, null, null, null,
+        null, null, null, null, null, null, null, null, null, null, null,
+        null, null, null
+        );
+
+        UserActivityLog::createUserActivityLog(APIConstants::NAME_GET, "Fetched all Service Prices");
+
+        return response()->json(
+                $all ,200);
+    }
+
+    //get a single service
+    public function getSingleService($id){   
+        
+        $all = ServicePrice::selectServicePrice($id, null, null, null, null, null, null, null,
+        null, null, null, null, null, null, null, null, null, null, null,
+        null, null, null
+        );
+
+        UserActivityLog::createUserActivityLog(APIConstants::NAME_GET, "Fetched Service Price with id: ".$all[0]['id']);
+
+        return response()->json(
+                $all ,200);
+    }
+    
+    //create a service
+    public function createService(Request $request){
+        $request->validate([
+            'service' => 'nullable|exists:services,name',
+            'department' => 'nullable|exists:departments,name',
+            'consultation_category' => 'nullable|exists:consultation_categories,name',
+            'clinic' => 'nullable|exists:clinics,name',
+            'payment_type' => 'nullable|exists:payment_types,name',
+            'scheme' => 'nullable|exists:schemes,name',
+            'scheme_type' => 'nullable|exists:scheme_types,name',
+            'consultation_type' => 'nullable|exists:consultation_types,name',
+            'visit_type' => 'nullable|exists:visit_types,name',
+            'doctor' => 'nullable|string', // Assuming doctor is an employee
+            'lab_test_type' => 'nullable|exists:lab_test_types,name',
+            'image_test_type' => 'nullable|exists:image_test_types,name',
+            'drug' => 'nullable|exists:drugs,name',
+            'brand' => 'nullable|exists:brands,name',
+            'branch' => 'nullable|exists:branches,name',
+            'building' => 'nullable|exists:buildings,name',
+            'wing' => 'nullable|exists:wings,name',
+            'ward' => 'nullable|exists:wards,name',
+            'office' => 'nullable|exists:offices,name',
+            'price' => 'nullable|numeric', // Price should be numeric
+            'price_applies_from' => 'nullable|date_format:H:i', // Valid time in 24-hour format
+            'price_applies_to' => 'nullable|date_format:H:i', // Valid time in 24-hour format
+            'duration' => 'nullable|numeric|regex:/^\d+(\.\d{1,4})?$/'
+        ]);
+
+        // Initialize variables for each entity
+        $service_id = $department_id = $consultation_category_id = $clinic_id = null;
+        $payment_type_id = $scheme_id = $scheme_type_id = $consultation_type_id = null;
+        $visit_type_id = $doctor_id = $lab_test_type_id = $image_test_type_id = null;
+        $drug_id = $brand_id = $branch_id = $building_id = $wing_id = null;
+        $ward_id = $office_id = null;
+
+        // Perform individual queries and assign the ids or null
+        if ($request->has('service')) {
+            $service = Service::where('name', $request->service)->first();
+            $service_id = $service ? $service->id : null;
+        }
+
+        if ($request->has('department')) {
+            $department = Department::where('name', $request->department)->first();
+            $department_id = $department ? $department->id : null;
+        }
+
+        if ($request->has('consultation_category')) {
+            $consultationCategory = ConsultationCategory::where('name', $request->consultation_category)->first();
+            $consultation_category_id = $consultationCategory ? $consultationCategory->id : null;
+        }
+
+        if ($request->has('clinic')) {
+            $clinic = Clinic::where('name', $request->clinic)->first();
+            $clinic_id = $clinic ? $clinic->id : null;
+        }
+
+        if ($request->has('payment_type')) {
+            $paymentType = PaymentType::where('name', $request->payment_type)->first();
+            $payment_type_id = $paymentType ? $paymentType->id : null;
+        }
+
+        if ($request->has('scheme')) {
+            $scheme = Scheme::where('name', $request->scheme)->first();
+            $scheme_id = $scheme ? $scheme->id : null;
+        }
+
+        if ($request->has('scheme_type')) {
+            $schemeType = SchemeTypes::where('name', $request->scheme_type)->first();
+            $scheme_type_id = $schemeType ? $schemeType->id : null;
+        }
+
+        if ($request->has('consultation_type')) {
+            $consultationType = ConsultationType::where('name', $request->consultation_type)->first();
+            $consultation_type_id = $consultationType ? $consultationType->id : null;
+        }
+
+        if ($request->has('visit_type')) {
+            $visitType = VisitType::where('name', $request->visit_type)->first();
+            $visit_type_id = $visitType ? $visitType->id : null;
+        }
+
+        if ($request->has('doctor')) {
+            $doctor = Employee::where('employee_code', $request->doctor)->first(); // Assuming employees are doctors
+            $doctor_id = $doctor ? $doctor->id : null;
+        }
+
+        if ($request->has('lab_test_type')) {
+            $labTestType = LabTestType::where('name', $request->lab_test_type)->first();
+            $lab_test_type_id = $labTestType ? $labTestType->id : null;
+        }
+
+        if ($request->has('image_test_type')) {
+            $imageTestType = ImageTestType::where('name', $request->image_test_type)->first();
+            $image_test_type_id = $imageTestType ? $imageTestType->id : null;
+        }
+
+        if ($request->has('drug')) {
+            $drug = Drug::where('name', $request->drug)->first();
+            $drug_id = $drug ? $drug->id : null;
+        }
+
+        if ($request->has('brand')) {
+            $brand = Brand::where('name', $request->brand)->first();
+            $brand_id = $brand ? $brand->id : null;
+        }
+
+        if ($request->has('branch')) {
+            $branch = Branch::where('name', $request->branch)->first();
+            $branch_id = $branch ? $branch->id : null;
+        }
+
+        if ($request->has('building')) {
+            $building = Building::where('name', $request->building)->first();
+            $building_id = $building ? $building->id : null;
+        }
+
+        if ($request->has('wing')) {
+            $wing = Wing::where('name', $request->wing)->first();
+            $wing_id = $wing ? $wing->id : null;
+        }
+
+        if ($request->has('ward')) {
+            $ward = Ward::where('name', $request->ward)->first();
+            $ward_id = $ward ? $ward->id : null;
+        }
+
+        if ($request->has('office')) {
+            $office = Office::where('name', $request->office)->first();
+            $office_id = $office ? $office->id : null;
+        }
+
+
+    
+        $created = Service::create([
+            'name' => $request->name,
+            'description' => $request->description,
+            'created_by' => User::getLoggedInUserId()
+        ]);
+
+        UserActivityLog::createUserActivityLog(APIConstants::NAME_CREATE, "Created a Service with name: ". $request->name);
+
+        return response()->json(
+                Service::selectServices(null, $request->name)
+            ,200);
+    }
+
+    //update a service
+    public function updateService(Request $request){
+        $request->validate([
+            'id' => 'required|exists:services,id',
+            'name' => 'required|string',
+            'description' => 'nullable|string|max:1000',
+        ]);
+
+        $existing = Service::where('name', $request->name)->get();
+
+        count($existing) > 0 && $existing[0]['id'] != $request->id ? throw new AlreadyExistsException(APIConstants::NAME_SERVICE) : null ;
+
+    
+        Service::where('id', $request->id)
+             ->update([
+                'name' => $request->name,
+                'description' => $request->description,
+                'updated_by' => User::getLoggedInUserId()
+        ]);
+
+        UserActivityLog::createUserActivityLog(APIConstants::NAME_CREATE, "Updated a Service with name: ". $request->name);
+
+        return response()->json(
+                Service::selectServices(null, $request->name)
+            ,200);
+
+    }
+
+    //approve a service
+    public function approveService($id){
+        
+
+        $existing = Service::selectServices($id, null);
+        
+        if(count($existing) == 0){
+            throw new NotFoundException(APIConstants::NAME_SERVICE);
+        }
+
+    
+        Service::where('id', $id)
+            ->update([
+                'approved_by' => User::getLoggedInUserId(),
+                'approved_at' => Carbon::now(),
+                'disabled_by' => null,
+                'disabled_at' => null
+            ]);
+
+        UserActivityLog::createUserActivityLog(APIConstants::NAME_APPROVE, "Approved a Service with id: ". $id);
+
+        return response()->json(
+                Service::selectServices($id, null)
+            ,200);
+    }
+
+    //disable a service
+    public function disableService($id){
+        
+
+        $existing = Service::selectServices($id, null);
+        
+        if(count($existing) == 0){
+            throw new NotFoundException(APIConstants::NAME_SERVICE);
+        }
+
+    
+        Service::where('id', $id)
+            ->update([
+                'approved_by' => null,
+                'approved_at' => null,
+                'disabled_by' => User::getLoggedInUserId(),
+                'disabled_at' => Carbon::now()
+            ]);
+
+        UserActivityLog::createUserActivityLog(APIConstants::NAME_DISABLE, "Disabled a Service with id: ". $id);
+
+        return response()->json(
+                Service::selectServices($id, null)
+            ,200);
+    }
+
+    //soft Delete a service
+    public function softDeleteService($id){
+        
+
+        $existing = Service::selectServices($id, null);
+        
+        if(count($existing) == 0){
+            throw new NotFoundException(APIConstants::NAME_SERVICE);
+        }
+
+    
+        Service::where('id', $id)
+            ->update([
+                'deleted_by' => User::getLoggedInUserId(),
+                'deleted_at' => Carbon::now()
+            ]);
+
+        UserActivityLog::createUserActivityLog(APIConstants::NAME_SOFT_DELETE, "Soft deleted a service with id: ". $id);
+
+        return response()->json(
+                Service::selectServices($id, null)
+            ,200);
+    }
+
+    // restore soft-Deleted a service
+    public function restoreSoftDeleteService($id){
+        
+
+        $existing = Service::where('id', $id)->whereNotNull('deleted_by')->get();
+        
+        if(count($existing) == 0){
+            throw new NotFoundException(APIConstants::NAME_SERVICE);
+        }
+
+    
+        Service::where('id', $id)
+            ->update([
+                'deleted_by' => null,
+                'deleted_at' => null
+            ]);
+
+        UserActivityLog::createUserActivityLog(APIConstants::NAME_RESTORE, "Restored Soft deleted a service with id: ". $id);
+
+        return response()->json(
+                Service::selectServices($id, null)
+            ,200);
+    }
+
+    //permanently Delete a service
+    public function permanentDeleteService($id){
+        
+
+        $existing = Service::where('id', $id)->get();
+        
+        if(count($existing) == 0){
+            throw new NotFoundException(APIConstants::NAME_SERVICE);
+        }
+
+    
+        Service::destroy($id);
+
+        UserActivityLog::createUserActivityLog(APIConstants::NAME_PERMANENT_DELETE, "Permanently deleted a Service with name: ". $existing[0]['name']);
+
+        return response()->json(
+                []
+            ,200);
+    }
+
 }
--- app/Models/Admin/ServiceRelated/ServicePrice.php
+++ app/Models/Admin/ServiceRelated/ServicePrice.php
@@ -156,4 +156,230 @@ class ServicePrice extends Model
         return $this->belongsTo(Office::class, 'office_id');
     }
 
+
+    //perform selection
+    public static function selectServicePrice($id, $service, $department, $consultation_category, $clinic, $payment_type, $scheme, $scheme_type,
+        $consultation_type, $visit_type, $doctor, $current_time, $duration, $lab_test_type, $image_test_type, $drug_id, $brand, $branch, $building,
+        $wing, $ward, $office
+        ){
+
+        $service_prices_query = ServicePrice::with([
+            'service:id,name',
+            'department:id,name',
+            'consultationCategory:id,name',
+            'clinic:id,name',
+            'paymentType:id,name',
+            'scheme:id,name',
+            'schemeType:id,name',
+            'consultationType:id,name',
+            'visitType:id,name',
+            'doctor:id,name',
+            'labTestType:id,name',
+            'imageTestType:id,name',
+            'drug:id,name',
+            'brand:id,name',
+            'branch:id,name',
+            'building:id,name',
+            'wing:id,name',
+            'ward:id,name',
+            'office:id,name',
+            'createdBy:id,email',
+            'updatedBy:id,email',
+            'approvedBy:id,email'
+        ])->whereNull('service_prices.deleted_by')
+          ->whereNull('service_prices.deleted_at');
+
+          // Filter by matching the provided arguments using LIKE for text fields
+            if ($service) {
+                $service_prices_query->whereHas('service', function ($query) use ($service) {
+                    $query->where('name', 'like', "%$service%");
+                });
+            }
+
+            if ($department) {
+                $service_prices_query->whereHas('department', function ($query) use ($department) {
+                    $query->where('name', 'like', "%$department%");
+                });
+            }
+
+            if ($consultation_category) {
+                $service_prices_query->whereHas('consultationCategory', function ($query) use ($consultation_category) {
+                    $query->where('name', 'like', "%$consultation_category%");
+                });
+            }
+
+            if ($clinic) {
+                $service_prices_query->whereHas('clinic', function ($query) use ($clinic) {
+                    $query->where('name', 'like', "%$clinic%");
+                });
+            }
+
+            if ($payment_type) {
+                $service_prices_query->whereHas('paymentType', function ($query) use ($payment_type) {
+                    $query->where('name', 'like', "%$payment_type%");
+                });
+            }
+
+            if ($scheme) {
+                $service_prices_query->whereHas('scheme', function ($query) use ($scheme) {
+                    $query->where('name', 'like', "%$scheme%");
+                });
+            }
+
+            if ($scheme_type) {
+                $service_prices_query->whereHas('schemeType', function ($query) use ($scheme_type) {
+                    $query->where('name', 'like', "%$scheme_type%");
+                });
+            }
+
+            if ($consultation_type) {
+                $service_prices_query->whereHas('consultationType', function ($query) use ($consultation_type) {
+                    $query->where('name', 'like', "%$consultation_type%");
+                });
+            }
+
+            if ($visit_type) {
+                $service_prices_query->whereHas('visitType', function ($query) use ($visit_type) {
+                    $query->where('name', 'like', "%$visit_type%");
+                });
+            }
+
+            if ($doctor) {
+                $service_prices_query->whereHas('doctor', function ($query) use ($doctor) {
+                    $query->where('employee_name', 'like', "%$doctor%")
+                            ->where('employee_code', 'like', "%$doctor%")
+                            ->where('ipnumber', 'like', "%$doctor%");
+                });
+            }
+
+            if ($lab_test_type) {
+                $service_prices_query->whereHas('labTestType', function ($query) use ($lab_test_type) {
+                    $query->where('name', 'like', "%$lab_test_type%");
+                });
+            }
+
+            if ($image_test_type) {
+                $service_prices_query->whereHas('imageTestType', function ($query) use ($image_test_type) {
+                    $query->where('name', 'like', "%$image_test_type%");
+                });
+            }
+
+            if ($drug_id) {
+                $service_prices_query->whereHas('drug', function ($query) use ($drug_id) {
+                    $query->where('name', 'like', "%$drug_id%");
+                });
+            }
+
+            if ($brand) {
+                $service_prices_query->whereHas('brand', function ($query) use ($brand) {
+                    $query->where('name', 'like', "%$brand%");
+                });
+            }
+
+            if ($branch) {
+                $service_prices_query->whereHas('branch', function ($query) use ($branch) {
+                    $query->where('name', 'like', "%$branch%");
+                });
+            }
+
+            if ($building) {
+                $service_prices_query->whereHas('building', function ($query) use ($building) {
+                    $query->where('name', 'like', "%$building%");
+                });
+            }
+
+            if ($wing) {
+                $service_prices_query->whereHas('wing', function ($query) use ($wing) {
+                    $query->where('name', 'like', "%$wing%");
+                });
+            }
+
+            if ($ward) {
+                $service_prices_query->whereHas('ward', function ($query) use ($ward) {
+                    $query->where('name', 'like', "%$ward%");
+                });
+            }
+
+            if ($office) {
+                $service_prices_query->whereHas('office', function ($query) use ($office) {
+                    $query->where('name', 'like', "%$office%");
+                });
+            }
+
+            // Filter by current time (check if current time is within 'price_applies_from' and 'price_applies_to')
+            if ($current_time) {
+                $service_prices_query->where(function ($query) use ($current_time) {
+                    $query->where('price_applies_from', '<=', $current_time)
+                        ->where('price_applies_to', '>=', $current_time);
+                });
+            }
+
+            // Filter by matching duration
+            if ($duration) {
+                $service_prices_query->where('duration', $duration);
+            }
+
+        if($id != null){
+            $service_prices_query->where('service_prices.id', $id);
+        }
+
+        else{
+            $paginated_service_prices = $service_prices_query->paginate(10);
+            
+            $paginated_service_prices->getCollection()->transform(function ($service_price) {
+                return ServicePrice::mapResponse($service_price);
+            });
+    
+            return $paginated_service_prices;
+        }
+
+        
+
+        
+        return $service_prices_query->get()->map(function ($service_price) {
+            $service_price_details = ServicePrice::mapResponse($service_price);
+
+            return $service_price_details;
+        });
+
+    }
+
+    private static function mapResponse($service_price){
+        return [
+            'id' => $service_price->id,
+            'service' => $service_price->service ? $service_price->service->name : null, // Check if service exists
+            'department' => $service_price->department ? $service_price->department->name : null, // Check if department exists
+            'consultation_category' => $service_price->consultationCategory ? $service_price->consultationCategory->name : null, // Check if consultationCategory exists
+            'clinic' => $service_price->clinic ? $service_price->clinic->name : null, // Check if clinic exists
+            'payment_type' => $service_price->paymentType ? $service_price->paymentType->name : null, // Check if paymentType exists
+            'scheme' => $service_price->scheme ? $service_price->scheme->name : null, // Check if scheme exists
+            'scheme_type' => $service_price->schemeType ? $service_price->schemeType->name : null, // Check if schemeType exists
+            'consultation_type' => $service_price->consultationType ? $service_price->consultationType->name : null, // Check if consultationType exists
+            'visit_type' => $service_price->visitType ? $service_price->visitType->name : null, // Check if visitType exists
+            'doctor_name' => $service_price->doctor ? $service_price->doctor->employee_name : null, // Check if doctor exists
+            'doctor_ipnumber' => $service_price->doctor ? $service_price->doctor->ipnumber : null,
+            'doctor_employee_code' => $service_price->doctor ? $service_price->doctor->employee_code : null,
+            'lab_test_type' => $service_price->labTestType ? $service_price->labTestType->name : null, // Check if labTestType exists
+            'image_test_type' => $service_price->imageTestType ? $service_price->imageTestType->name : null, // Check if imageTestType exists
+            'drug' => $service_price->drug ? $service_price->drug->name : null, // Check if drug exists
+            'brand' => $service_price->brand ? $service_price->brand->name : null, // Check if brand exists
+            'branch' => $service_price->branch ? $service_price->branch->name : null, // Check if branch exists
+            'building' => $service_price->building ? $service_price->building->name : null, // Check if building exists
+            'wing' => $service_price->wing ? $service_price->wing->name : null, // Check if wing exists
+            'ward' => $service_price->ward ? $service_price->ward->name : null, // Check if ward exists
+            'office' => $service_price->office ? $service_price->office->name : null, // Check if office exists
+            'price' => $service_price->price, // Service price
+            'price_applies_from' => $service_price->price_applies_from, // Price applies from date
+            'price_applies_to' => $service_price->price_applies_to, // Price applies to date
+            'duration' => $service_price->duration, // Duration
+            'created_by' => $service_price->createdBy ? $service_price->createdBy->email : null, // Created by user email
+            'created_at' => $service_price->created_at, // Created at timestamp
+            'updated_by' => $service_price->updatedBy ? $service_price->updatedBy->email : null, // Updated by user email
+            'updated_at' => $service_price->updated_at, // Updated at timestamp
+            'approved_by' => $service_price->approvedBy ? $service_price->approvedBy->email : null, // Approved by user email
+            'approved_at' => $service_price->approved_at, // Approved at timestamp
+        ];
+    }
+
+
 }
