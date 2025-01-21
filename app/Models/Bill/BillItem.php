e9403a3718ffcf04e94f65865ab0f63f06ba0eec
Sir-Dommy <dominickyengo2017@gmail.com>
2025-01-21 16:24:14 +0300

    creating Bill related models

---
+++ app/Models/Bill/BillItem.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Bill;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BillItem extends Model
+{
+    use HasFactory;
+}
