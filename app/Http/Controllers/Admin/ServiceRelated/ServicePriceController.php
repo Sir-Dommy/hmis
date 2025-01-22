55b78f56af518d48dd031a6432dac80b75f70d63
Sir-Dommy <dominickyengo2017@gmail.com>
2025-01-22 12:30:14 +0300

    debug single service price

--- app/Http/Controllers/Admin/ServiceRelated/ServicePriceController.php
+++ app/Http/Controllers/Admin/ServiceRelated/ServicePriceController.php
@@ -425,8 +425,9 @@ class ServicePriceController extends Controller
          );
 
          //$response_1 = json_decode($existing_price, true); // Decode the JSON response into an associative array
-
-         return $existing_price['data'];
+         DB::commit();
+         return $existing_price;
+         
          // Check if 'data' exists and is not empty
          if (isset($existing_price['data']) && !empty($existing_price['data'])) {
              $existing_id = $existing_price['data'][0]['id'] ?? null; // Extract the 'id' from the first object in 'data'
