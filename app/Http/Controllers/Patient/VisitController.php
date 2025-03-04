084406c42048a0a500c83f632f9e006ea195715c
Sir-Dommy <dominickyengo2017@gmail.com>
2025-03-04 10:43:28 +0300

    commenting existing visit validation before creating a new visit

--- app/Http/Controllers/Patient/VisitController.php
+++ app/Http/Controllers/Patient/VisitController.php
@@ -51,10 +51,10 @@ class VisitController extends Controller
         
         ]);
 
-        // ensure that there is no existing open visit for the patient...............
-        count(Visit::where('patient_id', $request->patient_id)
-                                ->where('open', 1)
-                                ->get()) > 0 ? throw new InputsValidationException("An open visit for patient ". $request->patient_id . " Already exists!!!!!!") : null;
+        // // ensure that there is no existing open visit for the patient...............
+        // count(Visit::where('patient_id', $request->patient_id)
+        //                         ->where('open', 1)
+        //                         ->get()) > 0 ? throw new InputsValidationException("An open visit for patient ". $request->patient_id . " Already exists!!!!!!") : null;
         
 
         $department = Department::where('name', $request->department)->get("id");
