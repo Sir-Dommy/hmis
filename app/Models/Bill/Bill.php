180c7a7190957c7077fc178830db3665ed0ada0c
Sir-Dommy <dominickyengo2017@gmail.com>
2025-03-04 11:19:34 +0300

    lets see mark up value 1.2

--- app/Models/Bill/Bill.php
+++ app/Models/Bill/Bill.php
@@ -301,6 +301,8 @@ class Bill extends Model
         print_r($single_service_price[0]['mark_up_type']);
 
         foreach($single_service_price as $service_price){
+
+            print_r($service_price->mark_up_type);
             if(isset($service_price['mark_up_type'])){
                 $service_price->mark_up_type == APIConstants::NAME_PERCENTAGE ? $selling_price_details['selling_price'] = $service_price->cost_price * ( 1 + ($service_price->mark_up_value/100)) : $selling_price_details['selling_price'] = $service_price->cost_price + $service_price->mark_up_value;
             }
