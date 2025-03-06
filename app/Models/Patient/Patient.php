6eb2428b1a7a9cc3e143c6f5436d6101f3645d7b
Sir-Dommy <dominickyengo2017@gmail.com>
2025-03-06 10:43:32 +0300

    lets see request query

--- app/Models/Patient/Patient.php
+++ app/Models/Patient/Patient.php
@@ -168,12 +168,12 @@ class Patient extends Model
         count($existing_employee[0]['departments']) < 1 ? throw new InHouseUnauthorizedException("You are not assigned to any department yet!!!") : null;
 
         foreach($existing_employee[0]['departments'] as $department){
-            echo $department->pivot->department_id;
             $patients_query->whereHas('visits.bills.billItems.serviceItem', function ($query) use ($department) {
                 $query->where('department_id', $department->pivot->department_id);
             });
         }
 
+        echo $patients_query->toSql();
 
         // else{
         //     $paginated_patients = $patients_query->paginate(10);
