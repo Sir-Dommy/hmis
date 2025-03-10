6c23b921a59fe349cd21cb9ae9cb0ff8cab74dd1
Sir-Dommy <dominickyengo2017@gmail.com>
2025-03-10 13:38:24 +0300

    logic to create a consultation

--- app/Http/Controllers/Doctor/ConsultationController.php
+++ app/Http/Controllers/Doctor/ConsultationController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers\Doctor;
 
+use App\Exceptions\InputsValidationException;
 use App\Http\Controllers\Controller;
 use App\Models\Admin\ConsultationType;
+use App\Models\Admin\PhysicalExaminationType;
+use App\Models\Admin\Symptom;
 use App\Models\Doctor\Consultation;
+use App\Models\Doctor\ConsultationPhysicalExaminationsJoin;
+use App\Models\Doctor\ConsultationSymptomsJoin;
+use App\Models\UserActivityLog;
+use App\Utils\APIConstants;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -28,17 +35,37 @@ class ConsultationController extends Controller
 
             DB::beginTransaction();
 
-            // $created = Consultation::create([
-            //     'visit_id'=>$request->visit_id,
-            //     'consultation_type_id'=>$request->$consultation_type_id,
-            //     'clinical_history'=>$request->clinical_history
-            // ]);
+            $created = Consultation::create([
+                'visit_id'=>$request->visit_id,
+                'consultation_type_id'=>$request->$consultation_type_id,
+                'clinical_history'=>$request->clinical_history
+            ]);
 
             // create chief complains
             foreach($request->chief_complains as $chief_complain){
-                        
+                
+                $existing_chief_complain = Symptom::where('name', $chief_complain)->get('id');
 
-                return $chief_complain;
+                count($existing_chief_complain) < 1 ? throw new InputsValidationException("Chief complain with name: " + $chief_complain + " does not exist!!!") : null;
+
+                ConsultationSymptomsJoin::create([
+                    'consultation_id' => $created->id,
+                    'symptom_id' => $existing_chief_complain[0]['id'],
+                ]);
+
+                
+            }
+            // create chief physical examinations
+            foreach($request->physical_examinations as $physical_examination){
+                
+                $existing_physical_examination = PhysicalExaminationType::where('name', $physical_examination)->get('id');
+
+                count($existing_physical_examination) < 1 ? throw new InputsValidationException("Physical examination with name: " + $physical_examination + " does not exist!!!") : null;
+
+                ConsultationPhysicalExaminationsJoin::create([
+                    'consultation_id' => $created->id,
+                    'physical_examination_id' => $existing_physical_examination[0]['id'],
+                ]);
                 
             }
 
@@ -54,8 +81,10 @@ class ConsultationController extends Controller
             throw new Exception($e);
         }
 
+        UserActivityLog::createUserActivityLog(APIConstants::NAME_CREATE, "Created a consultation: ". $created->id);
+
         return response()->json(
-            $consultation_type_id, 200
-        );
+            Consultation::selectConsultations($created->id)
+        ,200);
     }
 }
--- app/Models/Doctor/Consultation.php
+++ app/Models/Doctor/Consultation.php
@@ -2,13 +2,17 @@
 
 namespace App\Models\Doctor;
 
+use App\Models\Admin\ConsultationType;
 use App\Models\Admin\Diagnosis;
+use App\Utils\CustomUserRelations;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Consultation extends Model
 {
     use HasFactory;
+
+    use CustomUserRelations;
 
     protected $table = 'consultations';
 
@@ -23,6 +27,10 @@ class Consultation extends Model
         "deleted_at",
     ];
 
+    public function consultationType(){
+        return $this->belongsTo(ConsultationType::class, 'consultation_type_id');
+    }
+
     public function diagnosis(){
         return $this->belongsToMany(Diagnosis::class, 'consultation_diagnosis_join', 'consultation_id', 'diagnosis_id');
     }
@@ -33,6 +41,46 @@ class Consultation extends Model
 
     public function physicalExaminations(){
         return $this->belongsToMany(Diagnosis::class, 'consultations_physical_exam_join', 'consultation_id', 'physical_examination_id');
+    }
+
+
+    //perform selection
+    public static function selectConsultations($id){
+
+        $consultations_query = Consultation::with([
+            'consultationType:id,name',
+            'diagnosis:id,name',
+            'symptoms:id,name',
+            'physicalExaminations:id,name',
+            'createdBy:id,email',
+            'updatedBy:id,email',
+            'approvedBy:id,email'
+        ])->whereNull('consultations.deleted_by')
+          ->whereNull('consultations.deleted_at');
+
+        if($id != null){
+            $consultations_query->where('consultations.id', $id);
+        }
+
+
+
+        return $consultations_query->get()->map(function ($consultation) {
+            $consultations_details = [
+                'id' => $consultation->id,
+                'visit_id' => $consultation->visit_id,
+                'consultation_type' => $consultation->consultationType->name,
+                'clinical_history' => $consultation->clinical_history,
+                'diagnosis' => $consultation->diagnosis,
+                'symptoms' => $consultation->symptoms,
+                'physical_examinations' => $consultation->physicalExaminations,
+                
+            ];
+
+            $related_user =  CustomUserRelations::relatedUsersDetails($consultation);
+
+            return array_merge($consultations_details, $related_user);
+        });
+
     }
 
 }
