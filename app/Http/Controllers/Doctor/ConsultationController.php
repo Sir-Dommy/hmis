0a0d528618c74b766d04c0582dea97901d943a22
Sir-Dommy <dominickyengo2017@gmail.com>
2025-03-10 12:37:08 +0300

    lets see the consultaion type id

--- app/Http/Controllers/Doctor/ConsultationController.php
+++ app/Http/Controllers/Doctor/ConsultationController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Doctor;
 
 use App\Http\Controllers\Controller;
+use App\Models\Doctor\Consultation;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ConsultationController extends Controller
 {
@@ -13,5 +15,18 @@ class ConsultationController extends Controller
             'visit_id' => 'required|exists:visits,id',
             'consultation_type' => 'required|exists:consultation_types,name',
         ]);
+
+        $consultation_type_id = Consultation::where('name', $request->consultation_type)->first()->id;
+
+        // DB::beginTransaction();
+
+        // $created = Consultation::create([
+        //     'visit_id'=>$request->visit_id,
+        //     'consultation_type_id'=>R
+        // ]);
+
+        return response()->json(
+            $consultation_type_id, 200
+        );
     }
 }
