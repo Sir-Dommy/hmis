c53e12c82c862fc60d0d4260787dbf8bea75d922
Sir-Dommy <dominickyengo2017@gmail.com>
2025-03-10 11:06:23 +0300

    creating migrations for main service, appointment_follow_ups and visit documents and their related models

---
+++ database/migrations/2025_03_10_104632_create_visit_documents_table.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('visit_documents', function (Blueprint $table) {
+            $table->id();
+            $table->string("path");
+            $table->text("description")->nullable();
+            $table->unsignedBigInteger('created_by');
+            $table->unsignedBigInteger('updated_by')->nullable();
+            $table->unsignedBigInteger('approved_by')->nullable();
+            $table->timestamp('approved_at')->nullable();
+            $table->unsignedBigInteger('disabled_by')->nullable();
+            $table->timestamp('disabled_at')->nullable();
+            $table->unsignedBigInteger('deleted_by')->nullable();
+            $table->timestamp('deleted_at')->nullable();
+            $table->timestamps();
+
+            $table->foreign('created_by') // Column name
+                  ->references('id') // Target column in the parent table
+                  ->on('users') // Parent table
+                  ->onDelete('cascade');
+
+            $table->foreign('updated_by') // Column name
+                ->references('id') // Target column in the parent table
+                ->on('users') // Parent table
+                ->onDelete('cascade');
+
+            $table->foreign('approved_by') // Column name
+                ->references('id') // Target column in the parent table
+                ->on('users') // Parent table
+                ->onDelete('cascade');
+
+            $table->foreign('deleted_by') // Column name
+                ->references('id') // Target column in the parent table
+                ->on('users') // Parent table
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('visit_documents');
+    }
+};
