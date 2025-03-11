07b0e3b287a350cce7020fd9c982ef74abca50b8
Sir-Dommy <dominickyengo2017@gmail.com>
2025-03-11 10:12:34 +0300

    creating lab routes and adding them to the api routes

---
+++ routes/routeCollection/labRoutes.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\Doctor\ConsultationController;
+use App\Http\Controllers\Laboratory\OrderedTestsController;
+use App\Http\Controllers\Logs\LogsController;
+use Illuminate\Support\Facades\Route;
+
+Route::group(['prefix'=>'lab'], function(){
+
+    Route::post('create', [OrderedTestsController::class, 'createOrderTest']);
+
+});
