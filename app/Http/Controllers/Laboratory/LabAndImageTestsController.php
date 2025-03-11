da5dcacbee1a930687b167b21384990a690ef427
Sir-Dommy <dominickyengo2017@gmail.com>
2025-03-11 09:34:08 +0300

    creating image and lab tests controller

---
+++ app/Http/Controllers/Laboratory/LabAndImageTestsController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Laboratory;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LabAndImageTestsController extends Controller
+{
+    //
+}
