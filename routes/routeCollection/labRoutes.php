5ded698b4cecc9f3151190c1cb52d2962d2cba47
Sir-Dommy <dominickyengo2017@gmail.com>
2025-03-11 10:21:04 +0300

    renaming order test creation api route

--- routes/routeCollection/labRoutes.php
+++ routes/routeCollection/labRoutes.php
@@ -6,7 +6,10 @@ use App\Http\Controllers\Logs\LogsController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix'=>'lab'], function(){
+    Route::group(['prefix'=>'orderTests'], function(){
+        Route::post('create', [OrderedTestsController::class, 'createOrderTest']);
+    });
 
-    Route::post('create', [OrderedTestsController::class, 'createOrderTest']);
+    
 
 });
