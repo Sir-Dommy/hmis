b27c2b55df5ccbe8eb7aa805fa168ffb487d6f31
Sir-Dommy <dominickyengo2017@gmail.com>
2025-03-12 12:37:45 +0300

    lets see payment type

--- app/Http/Controllers/Pharmacy/PrescriptionController.php
+++ app/Http/Controllers/Pharmacy/PrescriptionController.php
@@ -101,7 +101,7 @@ class PrescriptionController extends Controller
 
             if($existing_visit[0]['payment_types']){
                 foreach($existing_visit[0]['payment_types'] as $visit_payment_type){
-                    return $visit_payment_type['payment_type']->name;
+                    return $visit_payment_type->payment_type;
                     if($visit_payment_type['name'] == APIConstants::NAME_CASH){
                         $payment_type_to_use = $visit_payment_type['name'];
 
