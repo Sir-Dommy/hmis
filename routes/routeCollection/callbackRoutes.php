edc0dcf6beb97fc02b4356f35902f49a6fc4f984
Sir-Dommy <dominickyengo2017@gmail.com>
2025-03-25 20:15:58 +0300

    making routes accept both post and get

--- routes/routeCollection/callbackRoutes.php
+++ routes/routeCollection/callbackRoutes.php
@@ -5,7 +5,10 @@ use App\Http\Controllers\Test\CallBactTestController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix'=>'callback'], function(){
-    Route::get('collect', [CallBactTestController::class, 'collect']);
-    Route::get('disburse', [CallBactTestController::class, 'disburse']);
+    Route::match(['get', 'post'], 'collect', [CallBactTestController::class, 'collect']);
+    Route::match(['get', 'post'], 'disburse', [CallBactTestController::class, 'disburse']);
+
+    // Route::get('collect', [CallBactTestController::class, 'collect']);
+    // Route::get('disburse', [CallBactTestController::class, 'disburse']);
     
 });
