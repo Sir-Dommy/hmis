848a37d78ae012272813c3382853a57ad3878e9d
Sir-Dommy <dominickyengo2017@gmail.com>
2025-04-29 14:33:23 +0300

    test mpesa payments

--- app/Http/Controllers/Bill/BillController.php
+++ app/Http/Controllers/Bill/BillController.php
@@ -7,5 +7,18 @@ use Illuminate\Http\Request;
 
 class BillController extends Controller
 {
-    //
+    //test mpesa payments
+    public function testMpesaPayment(Request $request)
+    {
+        $data = [
+            'amount' => 1,
+            'phone_number' => '254708123456',
+            'account_number' => '123456',
+            'transaction_type' => 'CustomerPayBillOnline',
+            'transaction_desc' => 'Test payment',
+            'callback_url' => url('/api/v1/bill/callback'),
+        ];
+
+        return response()->json($data);
+    }
 }
