73857266a61ec8e446d5d568e08cb1319fd3857f
Sir-Dommy <dominickyengo2017@gmail.com>
2025-05-10 23:26:21 +0300

    lets see all service item

--- app/Models/Bill/Bill.php
+++ app/Models/Bill/Bill.php
@@ -74,7 +74,7 @@ class Bill extends Model
             'visit.visitClinics.clinic:id,name',
             'reversedBy:id,email',
             'billItems:id,bill_id,service_item_id,one_item_selling_price,discount,quantity,unit,amount_paid,status,offer_status,description',
-            'billItems.serviceItem:id,expiry_date,price_applies_from,price_applies_to,duration',
+            'billItems.serviceItem',
             'billItems.serviceItem.department:id,name',
             'billItems.serviceItem.consultationCategory:id,name',
             'billItems.serviceItem.clinic:id,name',
