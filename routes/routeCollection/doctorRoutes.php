c62f58df5afbb42e4ec10580a5afc7ed8498df9e
Sir-Dommy <dominickyengo2017@gmail.com>
2025-05-11 06:38:31 +0300

    feat: add consultation update, retrieval, and deletion routes in doctorRoutes

--- routes/routeCollection/doctorRoutes.php
+++ routes/routeCollection/doctorRoutes.php
@@ -9,5 +9,12 @@ Route::group(['prefix'=>'doctor'], function(){
     Route::group(['prefix'=>'consultations'], function(){
 
         Route::post('create', [ConsultationController::class, 'createConsultation']);
+        Route::put('update', [ConsultationController::class, 'updateConsultation']);
+        Route::get('get', [ConsultationController::class, 'getConsultation']);
+        Route::get('/', [ConsultationController::class, 'getAllConsultations']);
+        Route::put('softDelete', [ConsultationController::class, 'softDeleteConsultation']);
+        Route::put('restore', [ConsultationController::class, 'restoreConsultation']);
+        Route::post('permanentlyDelete', [ConsultationController::class, 'permanentlyDelete']);
+        
     });
 });
